edda4d286f65f9e8e4d163f104dd04e497774ffb
Allan Liu <allanliu@v1041-wn-rt-b-170-81.campus-dynamic.uwaterloo.ca>
2023-11-08 00:56:36 -0500

    created Potential, Integral, Derivative (PID) controller header code. I included all function definitions that I think that we're gonna be required to use for the drug delivery robot. THe gyroscope wiring has already been figured out. The reason I made everything in terms of heading is because the rotations per minute of each motor actually doesn't matter. If they start to diverge, then the heading will also start to diverge, thus courses correction can be done by altering the voltage into each motor. That is why only heading really matters. Another big thing is to ensure that the velocity of the robot does not go above 1km/h. After these are done, the driver control system of the robot should be complete. Next would be to integrate it with the pathfinding algorithm of the robot.

--- control_system/pid.h
+++ control_system/pid.h
@@ -5,3 +5,9 @@ void headingControl(int* heading);
 
 // read input from gyroscope and return a pointer to a gloabl heading variable that stores the current heading
 int* currentHeading();
+
+// calculate current velocity based of accelerometer
+int currentVelocity(int acceleration);
+
+// turn left or right, 0 for left, 1 for right
+void turn(int direciton, int* heading);
