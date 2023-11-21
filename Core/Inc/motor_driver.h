8f25f00e368a556791846643f8bc532a59451bb3
Allan Liu <allanliu@v1041-wn-rt-b-221-227.campus-dynamic.uwaterloo.ca>
2023-11-21 17:04:30 -0500

    current code for imu and pid

---
+++ Core/Inc/motor_driver.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <math.h>
+#include "main.h"
+
+void motorLeftFWD(void);
+
+void motorLeftRVS(void);
+
+void motorRightOFF(void);
+
+void motorRightFWD(void);
+
+void motorRightRVS(void);
+
+void motorRightOFF(void);
---
+++ Core/Inc/mpu6050.h
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <math.h>
+#include "main.h"
+
+typedef struct {
+    int16_t accelX_RAW;
+    int16_t accelY_RAW;
+    int16_t accelZ_RAW;
+    double accelX;
+    double accelY;
+    double accelZ;
+
+    int16_t gyroX_RAW;
+    int16_t gyroY_RAW;
+    int16_t gyroZ_RAW;
+    double gyroX;
+    double gyroY;
+    double gyroZ;
+
+    int16_t temp_RAW;
+    double temp;
+
+    double pitchAngle;
+    double rollAngle;
+    double yawAngle;
+
+    double dt;
+    double normalizeTick;
+} MPU6050;
+
+int mpu6050Init(I2C_HandleTypeDef *hi2c);
+
+void readAccelData(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
+void readGyroData(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
+void readTempData(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
+void calculateAngleAccel(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
+void calculateAngleGyro(MPU6050* data);
+
+void readGyroGx(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
+void readAccelAx(I2C_HandleTypeDef *hi2c, MPU6050* data);
+
